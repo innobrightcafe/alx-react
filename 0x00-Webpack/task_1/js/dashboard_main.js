6e7024a68a2e575c419a76812341553660d48045
inosoft <innobrightcafe@gmail.com>
2023-07-27 04:07:15 -0400

    Webpack task1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,20 +1,40 @@
 import $ from 'jquery';
+import debounce from 'lodash/debounce';
 
-$(document).ready(function(){
-    //Fuction to add wlwments to the body of the page
-    function addElements(){
-        //paragraph elements
-        const paragraph1 = $('<p>').text('Holberton Dashboard');
-        const paragraph2 = $('<p>').text('Dashboard data for the students');
-        const button = $('<button>').text('Click here to get started');
-        const paragraph3 = $('<p>').attr('id', 'count');
-        const paragraph4 = $('<P>').text('Copyright - Holberton School');
-        $('body').append(paragraph1, paragraph2, button, paragraph3, paragraph4);
+document.addEventListener('DOMContentLoaded', function () {
+  // Function to update the counter and paragraph content
+  let count = 0;
 
-    }
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} click${count === 1 ? '' : 's'} on the button`);
+  }
 
-    // Call the function to add elements to the body of the page
+  // Bind the debounce function to the click event on the button
+  const button = $('button');
+  button.on('click', debounce(updateCounter, 1000));
+  
+  // Function to add elements to the body of the page
+  function addElements() {
+    // Paragraph element: Holberton Dashboard
+    const paragraph1 = $('<p>').text('Holberton Dashboard');
+
+    // Paragraph element: Dashboard data for the students
+    const paragraph2 = $('<p>').text('Dashboard data for the students');
+
+    // Button element with the text Click here to get started
+    const button = $('<button>').text('Click here to get started');
+
+    // Paragraph element with the id 'count'
+    const paragraph3 = $('<p>').attr('id', 'count');
+
+    // Another paragraph: Copyright - Holberton School
+    const paragraph4 = $('<p>').text('Copyright - Holberton School');
+
+    // Add the elements to the dashboard container
+    $('#dashboard-container').append(paragraph1, paragraph2, button, paragraph3, paragraph4);
+  }
+
+  // Call the function to add elements to the dashboard container
   addElements();
-})
-
-function undateCounter()
+});
