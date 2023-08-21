ef431b55ca40d113f20c0211e61e16f4834ffe4b
inosoft <innobrightcafe@gmail.com>
2023-08-21 19:36:34 -0400

    Javascript web pack

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,21 +1,7 @@
+// interesting jquery import thanks to babel
 import $ from 'jquery';
 
-// Function to add three different paragraphs to the page body
-function paragraphs() {
-  const body = $('body');
-
-  // Paragraph 1
-  const paragraph1 = $('<p>').text('Holberton Dashboard');
-  body.append(paragraph1);
-
-  // Paragraph 2
-  const paragraph2 = $('<p>').text('Dashboard data for the students');
-  body.append(paragraph2);
-
-  // Paragraph 3
-  const paragraph3 = $('<p>').text('Copyright - Holberton School');
-  body.append(paragraph3);
-}
-
-// Call the function to add paragraphs to the page body
-paragraphs();
+// add three different p tags to body
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
