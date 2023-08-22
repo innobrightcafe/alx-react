609c482be617d4cca7fefaf65fb0151dcf07599d
inosoft <innobrightcafe@gmail.com>
2023-08-22 02:45:21 -0400

    webpack

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,40 +1,20 @@
-import $ from 'jquery';
-import debounce from 'lodash/debounce';
+"use strict";
+import $ from "jquery";
+import _ from "lodash";
 
-document.addEventListener('DOMContentLoaded', function () {
-  // Function to update the counter and paragraph content
-  let count = 0;
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
+$("body").append("<p>Copyright - Holberton School</p>");
 
-  function updateCounter() {
-    count++;
-    $('#count').text(`${count} click${count === 1 ? '' : 's'} on the button`);
-  }
+const updateCounter = () => {
+  let times = $("#count").html() || 0;
+  $("button").on("click", () => {
+    times++;
+    $("#count").html(`${times} clicks on the button`);
+  });
+};
 
-  // Bind the debounce function to the click event on the button
-  const button = $('button');
-  button.on('click', debounce(updateCounter, 1000));
-  
-  // Function to add elements to the body of the page
-  function addElements() {
-    // Paragraph element: Holberton Dashboard
-    const paragraph1 = $('<p>').text('Holberton Dashboard');
-
-    // Paragraph element: Dashboard data for the students
-    const paragraph2 = $('<p>').text('Dashboard data for the students');
-
-    // Button element with the text Click here to get started
-    const button = $('<button>').text('Click here to get started');
-
-    // Paragraph element with the id 'count'
-    const paragraph3 = $('<p>').attr('id', 'count');
-
-    // Another paragraph: Copyright - Holberton School
-    const paragraph4 = $('<p>').text('Copyright - Holberton School');
-
-    // Add the elements to the dashboard container
-    $('#dashboard-container').append(paragraph1, paragraph2, button, paragraph3, paragraph4);
-  }
-
-  // Call the function to add elements to the dashboard container
-  addElements();
-});
+_.debounce(updateCounter, 500);
+updateCounter();
